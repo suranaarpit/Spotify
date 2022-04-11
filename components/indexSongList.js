59f4c64215fa94acb4719d4ea635a0ed2cbfd5a9
Arpit Surana <suranaarpit34@gmail.com>
2022-04-11 16:12:42 +0530

    bug fixed

---
+++ components/indexSongList.js
@@ -0,0 +1,48 @@
+const indexSongList = () => {
+    return `<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">03:44 <i id="0" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">03:05 <i id="1" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">04:46 <i id="2" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">04:03 <i id="3" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">03:51 <i id="4" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">02:48 <i id="5" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">04:04 <i id="6" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">04:11 <i id="7" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>
+<div class="songItem">
+    <img alt="1">
+    <span class="songName"></span>
+    <span class="songlistplay"><span class="timestamp">03:44 <i id="8" class="far songItemPlay fa-play-circle"></i> </span></span>
+</div>`
+}
+export default indexSongList;
---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+const mainNavbar = () =>{
+    return `<nav>
+    <ul>
+        <li class="brand"><img src="/Spotify/logo.png" alt="Spotify"> Spotify</li>
+        <li id="homelink">Home</li>
+        <li id="aboutLink">About</li>
+    </ul>
+</nav>
+`
+}
+export { mainNavbar };
